99a0b2e8c6ac6970f34bb6b668e7cd4a5d74aeaf
Saquib Irtiza <saquibirtiza@users.noreply.github.com>
2020-06-13 22:05:06 -0500

    Updates

---
+++ precache-manifest.01cdf652e6c3cb82570278da78a2d46f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4edd7b2b79270d47cb85c686705683b6",
+    "url": "/identifAI-app/index.html"
+  },
+  {
+    "revision": "ebaccdcfd3f6e11c33bf",
+    "url": "/identifAI-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3e01d8a95e849cb32e48",
+    "url": "/identifAI-app/static/css/main.a5f93440.chunk.css"
+  },
+  {
+    "revision": "ebaccdcfd3f6e11c33bf",
+    "url": "/identifAI-app/static/js/2.3688084b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/identifAI-app/static/js/2.3688084b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e01d8a95e849cb32e48",
+    "url": "/identifAI-app/static/js/main.ffccf581.chunk.js"
+  },
+  {
+    "revision": "959164b541364dc1e68f",
+    "url": "/identifAI-app/static/js/runtime-main.ad3ec194.js"
+  },
+  {
+    "revision": "b37dc6e49762c606ac6654cd06990572",
+    "url": "/identifAI-app/static/media/image.b37dc6e4.png"
+  }
+]);
